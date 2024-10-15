11d9e1ebcfa3e27658681c342be9d31e6214dcd1
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-10-14 21:24:49 -0300

    chore: next-auth and prisma

---
+++ lib/auth.ts
@@ -0,0 +1,48 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { db } from "./db";
+import EmailProvider from "next-auth/providers/email";
+import { Adapter } from "next-auth/adapters";
+import { createStripeCustomer } from "@/services/stripe/stripe";
+
+export const authOptions: AuthOptions = {
+    pages: {
+        signIn: "/auth",
+        signOut: "/auth",
+        error: "/auth",
+        verifyRequest: "/auth",
+        newUser: "/app"
+    },
+    adapter: PrismaAdapter(db) as Adapter,
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: parseInt(process.env.EMAIL_SERVER_PORT as string, 10),
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM
+        })
+    ],
+    callbacks: {
+        async session({session, user}) {
+            session.user = {
+                ...session.user,
+                id: user.id
+            } as any
+
+            return session
+        }
+    },
+    events: {
+        createUser: async (message) => {
+            await createStripeCustomer({
+                name: message.user.name as string, 
+                email: message.user.email as string,
+            })
+        }
+    }
+}
