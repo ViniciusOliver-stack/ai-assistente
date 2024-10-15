b2474996d13a20d7d8d70012d09381b4c832fcfa
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-10-14 21:33:33 -0300

    feat: component providers

---
+++ components/providers.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+import { usePathname } from "next/navigation"
+import Sidebar from "./sidebar"
+import { Header } from "./header"
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname()
+
+  return (
+    <SessionProvider>
+      {pathname !== "/auth" && pathname !== "/" ? (
+        <div className="flex h-screen">
+          <Sidebar />
+          <div className="flex-1 flex flex-col overflow-hidden">
+            {/* <Header /> */}
+            <main className="flex-1 overflow-x-hidden overflow-y-auto px-9 py-6 w-full">
+              {children}
+            </main>
+          </div>
+        </div>
+      ) : (
+        <main>{children}</main>
+      )}
+    </SessionProvider>
+  )
+}
+
+export default Provider
