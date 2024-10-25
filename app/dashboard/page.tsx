2faeb70c6d24d82f0773772260b4694a9e40fddf
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-10-25 08:35:59 -0300

    feat: create page dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { GroqChat } from "@/components/chatGroq"
+import Title from "@/components/ui/title"
+import { useSession } from "next-auth/react"
+
+export default function App() {
+  const { data: session } = useSession()
+
+  return (
+    <div>
+      <Title
+        title="Agentes"
+        description="Gerencie seus agentes para automatizar tarefas de forma inteligente"
+      />
+
+      <p>{session?.user?.email}</p>
+
+      <GroqChat />
+    </div>
+  )
+}
