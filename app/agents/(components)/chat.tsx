24fd3f594e4c480df1b708f8fc906c67615100c8
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-11-03 18:05:36 -0300

    fix: adjusting the feedback

--- app/agents/(components)/chat.tsx
+++ app/agents/(components)/chat.tsx
@@ -1,86 +1,167 @@
-import { useState } from "react"
+import { useEffect, useRef } from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
-type Message = {
-  id: number
-  text: string
-  sender: "client" | "ai"
-}
+import ReactMarkdown from "react-markdown"
+import { usePathname } from "next/navigation"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { useToast } from "@/hooks/use-toast"
+import { CopyIcon } from "lucide-react"
+import { useChatStore } from "@/store/useChatStore"
 
 export default function ChatLayout() {
-  const [messages, setMessages] = useState<Message[]>([
-    { id: 1, text: "Olá! Como você está?", sender: "ai" },
-    { id: 2, text: "Oi! Estou bem, e você?", sender: "client" },
-    { id: 3, text: "Também estou bem, obrigado!", sender: "ai" },
-  ])
-  const [newMessage, setNewMessage] = useState("")
+  const pathname = usePathname()
+  // const agentId = pathname.split("/")[2]
+  const messagesEndRef = useRef<HTMLDivElement>(null)
+  const { toast } = useToast()
 
-  const handleSendMessage = (e: React.FormEvent) => {
-    e.preventDefault()
-    if (newMessage.trim()) {
-      setMessages([
-        ...messages,
-        { id: messages.length + 1, text: newMessage, sender: "client" },
-      ])
-      setNewMessage("")
+  const {
+    messages,
+    isAIEnabled,
+    isLoading,
+    newMessage,
+    setIsAIEnabled,
+    setNewMessage,
+    sendManualMessage,
+  } = useChatStore()
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault()
+      sendManualMessage(newMessage)
     }
   }
 
+  const renderers = {
+    code({ inline, className, children }: any) {
+      const match = /language-(\w+)/.exec(className || "")
+      const language = match ? match[1] : "text"
+
+      const copyToClipboard = () => {
+        navigator.clipboard.writeText(children)
+        toast({
+          title: "Código copiado!",
+          description: "O código foi copiado para a área de transferência.",
+        })
+      }
+
+      return !inline ? (
+        <div className="relative">
+          <SyntaxHighlighter language={language} style={dracula}>
+            {String(children).replace(/\n$/, "")}
+          </SyntaxHighlighter>
+          <button
+            onClick={copyToClipboard}
+            className="absolute right-2 top-2 bg-gray-800 text-white py-1 px-2 rounded text-xs flex items-center gap-1"
+          >
+            <CopyIcon size={14} /> Copiar
+          </button>
+        </div>
+      ) : (
+        <code className={className}>{children}</code>
+      )
+    },
+  }
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [messages])
+
   return (
-    <div className="flex flex-col h-screen border rounded-lg overflow-hidden">
-      <div className="flex-grow p-4">
-        {messages.map((message) => (
+    <div className="flex flex-col h-[calc(100vh_-_7rem)] border rounded-lg overflow-hidden">
+      <Button onClick={() => setIsAIEnabled(!isAIEnabled)}>
+        {isAIEnabled ? "Desabilitar IA" : "Habilitar IA"}
+      </Button>
+
+      <div className="flex-grow p-4 overflow-auto">
+        {messages.map((message, index) => (
           <div
-            key={message.id}
+            key={index}
             className={`flex ${
-              message.sender === "client" ? "justify-end" : "justify-start"
+              message.sender === "ai" ? "justify-end" : "justify-start"
             } mb-4`}
           >
             <div
               className={`flex ${
-                message.sender === "client" ? "flex-row-reverse" : "flex-row"
+                message.sender === "ai" ? "flex-row-reverse" : "flex-row"
               } items-end`}
             >
               <Avatar className="w-8 h-8">
                 <AvatarImage
                   src={
-                    message.sender === "client"
+                    message.sender === "ai"
                       ? "/user-avatar.png"
                       : "/other-avatar.png"
                   }
                 />
                 <AvatarFallback>
-                  {message.sender === "client" ? "U" : "O"}
+                  {message.sender === "ai" ? "U" : "O"}
                 </AvatarFallback>
               </Avatar>
               <div
                 className={`mx-2 py-2 px-4 rounded-2xl ${
-                  message.sender === "client"
+                  message.sender === "ai"
                     ? "bg-primary text-primary-foreground"
                     : "bg-secondary text-secondary-foreground"
                 }`}
               >
-                {message.text}
+                {message.sender === "ai" ? (
+                  <ReactMarkdown components={renderers}>
+                    {message.text}
+                  </ReactMarkdown>
+                ) : (
+                  message.text
+                )}
               </div>
             </div>
           </div>
         ))}
+
+        {isLoading && (
+          <div className="flex justify-start mb-4">
+            <div className="flex items-end">
+              <Avatar className="w-8 h-8">
+                <AvatarImage src="/other-avatar.png" />
+                <AvatarFallback>O</AvatarFallback>
+              </Avatar>
+              <div className="m-2 p-2 rounded-2xl bg-secondary text-secondary-foreground">
+                <div className="dots-loading">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        <div ref={messagesEndRef} />
       </div>
 
-      <form onSubmit={handleSendMessage} className="p-4 bg-background">
-        <div className="flex space-x-2">
-          <Input
-            type="text"
-            placeholder="Digite sua mensagem..."
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            className="flex-grow"
-          />
-          <Button type="submit">Enviar</Button>
-        </div>
-      </form>
+      {!isAIEnabled && (
+        <form
+          className="p-4 bg-background"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="flex space-x-2">
+            <textarea
+              placeholder="Mensagem"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              className="flex-grow p-2 border rounded-md resize-none focus:outline-none"
+              rows={1}
+            />
+
+            <Button
+              onClick={() => sendManualMessage(newMessage)}
+              disabled={isLoading}
+            >
+              Enviar
+            </Button>
+          </div>
+        </form>
+      )}
     </div>
   )
 }
