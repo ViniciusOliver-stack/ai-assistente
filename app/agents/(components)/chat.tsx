0a4a566e91af493e5eb2cf0057027c6336bd22e5
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-11-17 17:16:10 -0300

    feat: Message display and layout improvement

--- app/agents/(components)/chat.tsx
+++ app/agents/(components)/chat.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import ReactMarkdown from "react-markdown"
@@ -6,8 +6,20 @@ import { usePathname } from "next/navigation"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import { useToast } from "@/hooks/use-toast"
-import { CopyIcon } from "lucide-react"
+import {
+  BotIcon,
+  BotOff,
+  CopyIcon,
+  MessageSquare,
+  SendHorizonalIcon,
+  Trash2Icon,
+  User2Icon,
+  X,
+} from "lucide-react"
 import { useChatStore } from "@/store/useChatStore"
+import { useChatListStore } from "@/store/useChatListStore"
+import { formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 export default function ChatLayout() {
   const pathname = usePathname()
@@ -20,15 +32,62 @@ export default function ChatLayout() {
     isAIEnabled,
     isLoading,
     newMessage,
+    currentChatId,
+    setCurrentChatId,
+    loadMessagesForChat,
     setIsAIEnabled,
     setNewMessage,
     sendManualMessage,
   } = useChatStore()
 
+  const { chats, activeChat, setActiveChat } = useChatListStore()
+
+  // Encontra o chat ativo e seu número de telefone
+  const activePhoneNumber = useMemo(() => {
+    const activeUser = chats.find((chat) => chat.id === activeChat)
+    return activeUser?.phoneNumber
+  }, [activeChat, chats])
+
+  // Sincroniza o chat ativo entre as duas stores
+  useEffect(() => {
+    if (activeChat && activeChat !== currentChatId) {
+      setCurrentChatId(activeChat)
+    }
+  }, [activeChat, currentChatId])
+
+  const handleChatSelect = (chatId: string) => {
+    setActiveChat(chatId)
+  }
+
+  const formatTimestamp = (timestamp: string) => {
+    return formatDistanceToNow(new Date(timestamp), {
+      addSuffix: true,
+      locale: ptBR,
+    })
+  }
+
+  console.log("Contato ativo", activeChat)
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault()
-      sendManualMessage(newMessage)
+      if (activePhoneNumber) {
+        sendManualMessage(activePhoneNumber, newMessage)
+        setNewMessage("")
+      }
+    }
+  }
+
+  const handleSendMessage = () => {
+    if (activePhoneNumber) {
+      sendManualMessage(newMessage, activePhoneNumber)
+    } else {
+      toast({
+        title: "Erro",
+        description:
+          "Não foi possível enviar a mensagem: número de telefone não encontrado",
+        variant: "destructive",
+      })
     }
   }
 
@@ -67,100 +126,195 @@ export default function ChatLayout() {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
   }, [messages])
 
-  return (
-    <div className="flex flex-col h-[calc(100vh_-_7rem)] border rounded-lg overflow-hidden">
-      <Button onClick={() => setIsAIEnabled(!isAIEnabled)}>
-        {isAIEnabled ? "Desabilitar IA" : "Habilitar IA"}
-      </Button>
+  function formatTimestampToTime(timestamp: string): string {
+    return new Date(timestamp).toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    })
+  }
 
-      <div className="flex-grow p-4 overflow-auto">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              message.sender === "ai" ? "justify-end" : "justify-start"
-            } mb-4`}
-          >
+  if (chats.length === 0) {
+    return (
+      <div className="h-full flex flex-col items-center justify-center text-gray-500">
+        <MessageSquare className="w-12 h-12 mb-2" />
+        <p className="text-sm">Nenhuma conversa ainda</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex h-[calc(100vh_-_7rem)] border rounded-lg overflow-hidden">
+      {/* Lista de chats */}
+      <div className="w-1/4 bg-white border-r border-gray-200 p-4">
+        <div className="h-[calc(100vh-8rem)] overflow-y-auto">
+          {chats.map((chat) => (
             <div
-              className={`flex ${
-                message.sender === "ai" ? "flex-row-reverse" : "flex-row"
-              } items-end`}
+              key={chat.id}
+              className={`mb-4 p-3 hover:bg-gray-100 rounded-lg cursor-pointer ${
+                activeChat === chat.id ? "bg-gray-100" : ""
+              }`}
+              onClick={() => handleChatSelect(chat.id)}
             >
-              <Avatar className="w-8 h-8">
-                <AvatarImage
-                  src={
-                    message.sender === "ai"
-                      ? "/user-avatar.png"
-                      : "/other-avatar.png"
-                  }
-                />
-                <AvatarFallback>
-                  {message.sender === "ai" ? "U" : "O"}
-                </AvatarFallback>
-              </Avatar>
-              <div
-                className={`mx-2 py-2 px-4 rounded-2xl ${
-                  message.sender === "ai"
-                    ? "bg-primary text-primary-foreground"
-                    : "bg-secondary text-secondary-foreground"
-                }`}
-              >
-                {message.sender === "ai" ? (
-                  <ReactMarkdown components={renderers}>
-                    {message.text}
-                  </ReactMarkdown>
-                ) : (
-                  message.text
+              <div className="flex justify-between items-center mb-1">
+                <span className="font-semibold">{chat.name}</span>
+                <div className="flex items-center gap-2">
+                  <span className="text-xs text-gray-500">
+                    {formatTimestamp(chat.timestamp)}
+                  </span>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <p className="text-sm text-gray-600 truncate">
+                  {chat.lastMessage}
+                </p>
+
+                {chat.unreadCount > 0 && (
+                  <span className="bg-blue-500 text-primary-foreground text-xs rounded-full px-2 flex items-center">
+                    {chat.unreadCount}
+                  </span>
                 )}
               </div>
             </div>
-          </div>
-        ))}
-
-        {isLoading && (
-          <div className="flex justify-start mb-4">
-            <div className="flex items-end">
-              <Avatar className="w-8 h-8">
-                <AvatarImage src="/other-avatar.png" />
-                <AvatarFallback>O</AvatarFallback>
-              </Avatar>
-              <div className="m-2 p-2 rounded-2xl bg-secondary text-secondary-foreground">
-                <div className="dots-loading">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-
-        <div ref={messagesEndRef} />
+          ))}
+        </div>
       </div>
 
-      {!isAIEnabled && (
-        <form
-          className="p-4 bg-background"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <div className="flex space-x-2">
-            <textarea
-              placeholder="Mensagem"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              onKeyDown={handleKeyDown}
-              className="flex-grow p-2 border rounded-md resize-none focus:outline-none"
-              rows={1}
-            />
-
-            <Button
-              onClick={() => sendManualMessage(newMessage)}
-              disabled={isLoading}
-            >
-              Enviar
-            </Button>
+      {/* Área principal do chat */}
+      {currentChatId ? (
+        <div className="flex-1 flex flex-col">
+          {/* Cabeçalho do chat */}
+          <div className="bg-white p-4 shadow-sm flex justify-end items-center">
+            <div className="space-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => setIsAIEnabled(!isAIEnabled)}
+              >
+                {isAIEnabled ? (
+                  <BotOff className="w-4 h-4 mr-2" />
+                ) : (
+                  <BotIcon className="w-4 h-4 mr-2" />
+                )}
+                {isAIEnabled ? "Desabilitar IA" : "Habilitar IA"}
+              </Button>
+              <Button variant="outline" size="sm">
+                <X className="w-4 h-4 mr-2" />
+                Encerrar chamado
+              </Button>
+              <Button
+                variant="outline"
+                size="sm"
+                className="hover:bg-red-400 hover:text-white"
+              >
+                <Trash2Icon className="w-4 h-4 mr-2" />
+                Deletar conversa
+              </Button>
+            </div>
           </div>
-        </form>
+
+          {/* Mensagens do chat */}
+          <div className="flex-1 px-4  space-y-4">
+            <div className="flex-1 p-4 overflow-y-auto max-h-[calc(100vh-18rem)]">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`flex ${
+                    message.sender === "ai" ? "justify-end" : "justify-start"
+                  } mb-4`}
+                >
+                  <div
+                    className={`flex ${
+                      message.sender === "ai" ? "flex-row-reverse" : "flex-row"
+                    } items-end`}
+                  >
+                    <Avatar className="w-8 h-8">
+                      <AvatarImage
+                        src={
+                          message.sender === "ai"
+                            ? "/user-avatar.png"
+                            : "/other-avatar.png"
+                        }
+                      />
+                      <AvatarFallback>
+                        <User2Icon size={18} />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div
+                      className={`mx-2 py-2 px-4 rounded-2xl ${
+                        message.sender === "ai"
+                          ? "bg-primary text-primary-foreground"
+                          : "bg-secondary text-secondary-foreground"
+                      }`}
+                    >
+                      {message.sender === "ai" ? (
+                        <ReactMarkdown components={renderers}>
+                          {message.text}
+                        </ReactMarkdown>
+                      ) : (
+                        message.text
+                      )}
+
+                      <div className="text-[10px] text-gray-500 text-right mt-1">
+                        {formatTimestampToTime(message.timestamp)}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              {isLoading && (
+                <div className="flex justify-start mb-4">
+                  <div className="flex items-end">
+                    <Avatar className="w-8 h-8">
+                      <AvatarImage src="/other-avatar.png" />
+                      <AvatarFallback>O</AvatarFallback>
+                    </Avatar>
+                    <div className="m-2 p-2 rounded-2xl bg-secondary text-secondary-foreground">
+                      <div className="dots-loading">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              <div ref={messagesEndRef} />
+            </div>
+          </div>
+
+          {/* Input de mensagem */}
+          {!isAIEnabled && activePhoneNumber && (
+            <div className=" p-4 border-t border-gray-200">
+              <form className="" onSubmit={(e) => e.preventDefault()}>
+                <div className="flex space-x-2 items-center">
+                  <textarea
+                    placeholder="Mensagem"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    className="flex-grow p-2 border rounded-md resize-none focus:outline-none"
+                    rows={1}
+                  />
+
+                  <Button
+                    onClick={handleSendMessage}
+                    disabled={isLoading || !newMessage.trim()}
+                  >
+                    Enviar
+                    <SendHorizonalIcon className="w-4 h-4 ml-2" />
+                  </Button>
+                </div>
+              </form>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex-1 flex items-center justify-center text-gray-500">
+          <p>Selecione um chat para começar</p>
+        </div>
       )}
     </div>
   )
