e900997e1da246d84ca9672a39b1bd22f0044576
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-12-12 22:20:21 -0300

    feat: link correction and styling

--- components/sidebar-mobile.tsx
+++ components/sidebar-mobile.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Select,
   SelectContent,
@@ -11,50 +13,115 @@ import {
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { HomeIcon, Menu, SettingsIcon } from "lucide-react"
+import { HomeIcon, Menu, SettingsIcon, User } from "lucide-react"
 import Link from "next/link"
 import { ButtonSignOut } from "./ui/button-signout"
+import Image from "next/image"
+import useTeamStore from "@/store/team-store"
+import { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
+import { getTeams } from "@/app/_actions/get-teams"
+import { Team } from "@prisma/client"
 
 export function SidebarMobile() {
+  const [teams, setTeams] = useState<Team[]>([])
+  const [selectedTeam, setSelectedTeam] = useState<string>("")
+  const pathname = usePathname()
+  const pathnameContains = (path: string) => pathname.includes(path)
+  const setSelectedTeamId = useTeamStore((state) => state.setSelectedTeamId)
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      try {
+        const fetchedTeams = await getTeams()
+        setTeams(fetchedTeams)
+        // Seleciona a primeira equipe automaticamente
+        if (fetchedTeams.length >= 1) {
+          const firstTeamId = fetchedTeams[0].id
+          setSelectedTeam(firstTeamId)
+          setSelectedTeamId(firstTeamId)
+        }
+      } catch (error) {
+        console.error("Erro ao obter equipes:", error)
+      }
+    }
+
+    fetchTeams()
+  }, [setSelectedTeamId])
+
   return (
     <aside className="block md:hidden">
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger className="px-4 py-3">
           <Menu size={22} />
         </SheetTrigger>
-        <SheetContent className="bg-zinc-900 border-none flex flex-col justify-between">
+        <SheetContent className="border-none flex flex-col justify-between">
           <div>
-            <div className="py-5">
-              <Select>
+            <div className="h-12">
+              <Image src="/logo.svg" alt="logo" width={120} height={120} />
+            </div>
+            <div className="pb-5">
+              <Select
+                value={selectedTeam}
+                onValueChange={(value) => {
+                  setSelectedTeam(value)
+                  setSelectedTeamId(value)
+                }}
+              >
                 <SelectTrigger>
-                  <SelectValue placeholder="Empresas" />
+                  <SelectValue placeholder="Selecione a equipe" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="Symplus">Symplus</SelectItem>
-                  <SelectItem value="Suporte 5 estrelas">
-                    Suporte 5 estrelas
-                  </SelectItem>
-                  <SelectItem value="system">System</SelectItem>
+                  {teams.map((team) => (
+                    <SelectItem key={team.id} value={team.id}>
+                      {team.name}
+                    </SelectItem>
+                  ))}
                 </SelectContent>
               </Select>
             </div>
 
             <nav>
               <SheetClose
-                className="flex items-center gap-2 p-2 text-gray-300 hover:bg-neutral-600 text-sm rounded-md"
+                className="flex items-center gap-2 p-2 text-neutral-900 hover:bg-neutral-600 text-sm rounded-md"
                 asChild
               >
-                <Link href="/app">
+                <Link
+                  href="/dashboard"
+                  className={`${
+                    pathnameContains("/config") ? "text-blue-500" : ""
+                  } flex items-center gap-2 p-2 hover:bg-neutral-600 text-sm rounded-md`}
+                >
                   <HomeIcon size={18} />
-                  Início
+                  Dashboard
                 </Link>
               </SheetClose>
 
               <SheetClose
-                className="flex items-center gap-2 p-2 text-gray-300 hover:bg-neutral-600 text-sm rounded-md"
+                className="flex items-center gap-2 p-2 text-neutral-900 hover:bg-neutral-600 text-sm rounded-md"
                 asChild
               >
-                <Link href="/config">
+                <Link
+                  href="/agents"
+                  className={`${
+                    pathnameContains("/agents") ? "text-blue-500" : ""
+                  } flex items-center gap-2 p-2 hover:bg-neutral-600 text-sm rounded-md`}
+                >
+                  <User size={18} />
+                  Agentes
+                </Link>
+              </SheetClose>
+
+              <SheetClose
+                className="flex items-center gap-2 p-2 text-neutral-900 hover:bg-neutral-600 text-sm rounded-md"
+                asChild
+              >
+                <Link
+                  href="/config/profile"
+                  className={`${
+                    pathnameContains("/config") ? "text-blue-500" : ""
+                  } flex items-center gap-2 p-2 hover:bg-neutral-600 text-sm rounded-md`}
+                >
                   <SettingsIcon size={18} />
                   Configurações
                 </Link>
