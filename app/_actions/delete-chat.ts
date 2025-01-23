ff3e64901d59103312f2bc2da034637a3b58e690
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2025-01-23 17:41:19 -0300

    feat: delete chat id

---
+++ app/_actions/delete-chat.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { db } from "@/lib/db"
+import { revalidatePath } from "next/cache"
+
+export async function DeleteChat(conversationId: string) {
+    try {
+        await db.message.deleteMany({
+            where: {
+                conversationId: conversationId
+            },
+        })
+
+        await db.conversationParticipant.deleteMany({
+            where: {
+                conversationId: conversationId
+            }
+        })
+
+        await db.conversation.delete({
+            where: {
+                id: conversationId
+            }
+        })
+
+        revalidatePath("/agents")
+
+        return {success: true}
+    } catch (error) {
+        console.log("Error deleing chat: ", error)
+        return {success: false, error: "Failed to delete chat"}
+    }
+}
