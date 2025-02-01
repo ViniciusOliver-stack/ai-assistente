af66639cf23f34e665381056ecda6f9d4ddbbf38
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2025-02-01 16:48:59 -0300

    feat: componente skeleton loader

---
+++ components/animations/skeleton-loader.tsx
@@ -0,0 +1,7 @@
+export const SkeletonLoader = () => (
+  <div className="animate-pulse">
+    <div className="h-8 bg-gray-200 rounded w-1/4 mb-4"></div>
+    <div className="h-4 bg-gray-200 rounded w-1/2 mb-8"></div>
+    <div className="h-32 bg-gray-200 rounded-lg w-full"></div>
+  </div>
+)
