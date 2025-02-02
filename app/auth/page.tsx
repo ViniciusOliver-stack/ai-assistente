2b2284d7f37f8a2b49e440be83ec002e403e2b8c
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2025-02-02 19:01:28 -0300

    feat: style auth form

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -8,6 +8,7 @@ import { useToast } from "@/hooks/use-toast"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
 
 export default function Auth() {
   const [email, setEmail] = useState<null | string>(null)
@@ -15,11 +16,23 @@ export default function Auth() {
   const year = date.getFullYear()
 
   const { toast } = useToast()
+  const { theme } = useTheme()
+
+  const getLogoSrc = () => {
+    switch (theme) {
+      case "light":
+        return "/logo.svg"
+      case "dark":
+        return "/logo-white.svg"
+      default:
+        return "/logo-white.svg"
+    }
+  }
 
   async function SignInWithEmail() {
     const signInResult = await signIn("email", {
       email: email,
-      callbackUrl: "/app",
+      callbackUrl: "/dashboard",
       redirect: false,
     })
 
@@ -42,13 +55,13 @@ export default function Auth() {
   return (
     <div className="flex flex-1 flex-col justify-center items-center max-w-full h-screen select-none">
       <div className="flex-1 flex flex-col w-full justify-center items-center gap-6 transition-all ">
-        <Image src="/logo.svg" alt="logo rubnik" width={150} height={150} />
-        <div className="flex flex-col w-[380px] max-w-[90%] py-8 px-6 gap-6 rounded-lg bg-neutral-200/50 transition-all">
+        <Image src={getLogoSrc()} alt="logo rubnik" width={150} height={150} />
+        <div className="flex flex-col w-[380px] max-w-[90%] py-8 px-6 gap-6 rounded-lg bg-neutral-200/50 dark:bg-white transition-all">
           <form action={SignInWithEmail} className="flex flex-col gap-4">
             <div className="flex flex-col gap-2">
               <Label
                 htmlFor="email"
-                className="text-sm font-light text-neutral-600"
+                className="text-sm font-normal text-neutral-600 dark:text-neutral-700"
               >
                 Digite seu email
               </Label>
@@ -60,10 +73,18 @@ export default function Auth() {
                 placeholder="seu@email.com"
               />
             </div>
-            <Button type="submit" className="w-full">
+            <Button
+              type="submit"
+              className="w-full dark:bg-neutral-900 dark:text-white dark:hover:bg-blue-500 hover:bg-blue-500 hover:text-white"
+            >
               Entrar
             </Button>
           </form>
+        </div>
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
+          Ao clicar em entrar, você concorda com nossos{" "}
+          <a href="#">Termos e serviços</a> e{" "}
+          <a href="#">Política de Privacidade</a>.
         </div>
       </div>
       <footer className="text-sm text-neutral-500 flex flex-col items-center gap-1 mb-8">
